ed3e9ee2662115a0210b067c4c0cd5041c4afc94
smanish01 <manish.shukla@built.io>
2018-06-26 18:56:49 +0530

    twittertest

---
+++ tweet.js
@@ -0,0 +1,14 @@
+var Twitter = require('twitter');
+var client = new Twitter({
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+  });
+
+
+  client.post('statuses/update', {status: 'test1'},  function(error, tweet, response) {
+    if(error) throw error;
+    console.log(tweet);  // Tweet body.
+    console.log(response);  // Raw response object.
+  });   
